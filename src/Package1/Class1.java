2dfeac9616ccfe0d5818476e8f271eb200d8825d
Suchita1111 <suchitadhavane@gmail.com>
2023-09-20 00:34:52 +0530

    Inheritance-Initial Commit

---
+++ src/Package1/Class1.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class Class1 {
+    public void method1()
+    {
+        System.out.println("Method1");
+    }
+    public void method2()
+    {
+        System.out.println("Method2");
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/Package1/Class2.java
@@ -0,0 +1,11 @@
+package Package1;
+
+public class Class2 extends Class1 {
+
+    public static void main(String[] args) {
+        Class2 obj1 = new Class2();
+        obj1.method1();
+        obj1.method2();
+    }
+
+}
